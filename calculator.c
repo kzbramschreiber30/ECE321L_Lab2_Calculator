f890a8501b106e398ba93cf28dc207c28326cc39
kzbramschreiber30 <kyle.bramschreiber@siu.edu>
2025-09-17 21:45:53 -0500

    Implemented division feature

--- calculator.c
+++ calculator.c
@@ -3,6 +3,7 @@
 void add(float a, float b);
 void sub(float a, float b);
 void mult(float a, float b);
+void div(float a, float b);
 int main() {
     char op;
     float num1, num2;
@@ -11,6 +12,7 @@ int main() {
     add(num1, num2);
     dif(num1, num2);
     mult(num1, num2);
+    div(num1, num2);
 }
 void add (float a, float b){
     float sum;
@@ -26,4 +28,14 @@ void mult(float a, float b){
     float product;
     product = a * b;
     printf("The product is %f", product);
+}
+void div(float a, float b){
+    float quotient;
+    if (b != 0){
+        quotient = a / b;
+    } 
+    else{
+        printf("Divide by Zero Error! \n");
+    }  
+    printf("The quotient is %f", quotient);
 }
