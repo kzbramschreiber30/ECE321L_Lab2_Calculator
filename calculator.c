5ed48362d208a5b901e731050e874b7ff5fe69f9
kzbramschreiber30 <kyle.bramschreiber@siu.edu>
2025-09-17 21:33:45 -0500

    Implemented subtraction feature

--- calculator.c
+++ calculator.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include <float.h>
 void add(int a, int b);
+void sub(int a, int b);
 int main() {
     char op;
     int num1, num2;
-    num1 = 2;
-    num2 = 4;
+    num1 = 6;
+    num2 = 7;
     add(num1, num2);
+    dif(num1, num2);
 }
 void add (int a, int b){
     int sum;
     sum = a + b;
     printf("The sum is %i",sum);
+}
+void sub (int a, int b){
+    int dif;
+    dif = a - b;
+    printf("The difference is %i", dif);
 }
