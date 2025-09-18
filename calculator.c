86d361d33abab0b98c04c2f2a74e92da1c1de2e1
kzbramschreiber30 <kyle.bramschreiber@siu.edu>
2025-09-17 21:30:56 -0500

    Implemented addition feature

--- calculator.c
+++ calculator.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <float.h>
-
+void add(int a, int b);
 int main() {
-    
+    char op;
+    int num1, num2;
+    num1 = 2;
+    num2 = 4;
+    add(num1, num2);
+}
+void add (int a, int b){
+    int sum;
+    sum = a + b;
+    printf("The sum is %i",sum);
 }
